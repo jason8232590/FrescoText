064d6094b40bda8cad42e73d0566bf69c45b0b7d
jason8232590 <734975410@qq.com>
2016-09-19 22:24:59 +0800

    第六次提交

--- FrescoText0917/app/src/main/java/com/example/administrator/frescotext0917/MainActivity.java
+++ FrescoText0917/app/src/main/java/com/example/administrator/frescotext0917/MainActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private SimpleDraweeView draweeView;
+    private Button btToRoundConner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +21,38 @@ public class MainActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         findViewById(R.id.bt_load_img).setOnClickListener(this);
         draweeView = (SimpleDraweeView) findViewById(R.id.my_image_view);
+        btToRoundConner = (Button) findViewById(R.id.bt_to_round_conner);
+        GenericDraweeHierarchyBuilder builder =
+                new GenericDraweeHierarchyBuilder(getResources());
+//        GenericDraweeHierarchy hierarchy = builder
+//                .setFadeDuration(300)
+//                .setPlaceholderImage(new MyCustomDrawable())
+//                .setBackgrounds(backgroundList)
+//                .setOverlays(overlaysList)
+//                .build();
+
+        GenericDraweeHierarchy hierarchy = builder
+        .setFadeDuration(2000)
+        .build();
+        draweeView.setHierarchy(hierarchy);
+
     }
 
     @Override
     public void onClick(View view) {
-        System.out.println("xcqw 第五次提交");
-        System.out.println("xcqw 第四次提交");
-        System.out.println("xcqw 点到我了4");
-        System.out.println("xcqw 点到我了2");
-        System.out.println("xcqw 点到我了1");
-        Uri uri = Uri.parse("http://test2.img.hiwemeet.com/pic/240-240-5e5ad0765f3ef8ac13d7be2411e90cb7/0");
-        draweeView.setImageURI(uri);
+        if(R.id.bt_load_img == view.getId()) {
+            System.out.println("xcqw 点到我了1222222222222");
+            Uri uri = Uri.parse("http://test2.img.hiwemeet.com/pic/240-240-5e5ad0765f3ef8ac13d7be2411e90cb7/0");
+            draweeView.setImageURI(uri);
+        }
+    }
+
+    public void toRoundConner(View view){
+        System.out.println("xcqw 变成圆角");
+        //报错
+//        GenericDraweeHierarchy hierarchy = draweeView.getHierarchy();
+//        RoundingParams roundingParams = hierarchy.getRoundingParams();
+//        roundingParams.setCornersRadius(10);
+//        hierarchy.setRoundingParams(roundingParams);
     }
 }
