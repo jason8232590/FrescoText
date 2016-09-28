e8b90cc0ac595a1bd475af9be162303eadab2c90
jason8232590 <734975410@qq.com>
2016-09-28 08:05:48 +0800

    V1.0.1提交

--- FrescoText0917/app/src/main/java/com/example/administrator/frescotext0917/MainActivity.java
+++ FrescoText0917/app/src/main/java/com/example/administrator/frescotext0917/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     public void toRoundConner(View view){
+        System.out.println("xcqw 我是V1.1");
         System.out.println("xcqw 变成圆角");
         //报错
 //        GenericDraweeHierarchy hierarchy = draweeView.getHierarchy();
